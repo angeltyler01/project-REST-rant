47f7b758aa289b66b4db3c33fb7f2b366dad2d54
Angel Tyler <angeltyler@Angels-MBP.attlocal.net>
2022-08-05 11:14:58 -0700

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv') .config()
+
 // require express
 const express = require('express')
 
@@ -9,5 +11,10 @@ app.get('/', function(req,res) {
     res.send('Welcome')
 })
 
+//Wildcard route
+app.get('*', (req, res) =>{
+    res.status(404) .send('<h1>404 Page</h1>')
+})
+
 //listen for connections
-app.listen(4000)
+app.listen(process.env.PORT)
