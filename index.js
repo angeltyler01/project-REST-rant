bd16d3d2f141d6a9a2389675eae23af4e109562d
Angel Tyler <angeltyler@Angels-MBP.attlocal.net>
2022-08-05 11:28:15 -0700

    Adding places controller

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const express = require('express')
 //initialize express
 const app = express()
 
+//import router from places.js
+app.use('/places', require('./controllers/places'))
+
 //creating homepage route
 app.get('/', function(req,res) {
     res.send('Welcome')
