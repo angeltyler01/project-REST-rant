ea81adf7f627f7e6118797567b2aec9dee45cd39
Angel Tyler <angeltyler@Angels-MBP.attlocal.net>
2022-08-06 10:06:43 -0700

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv') .config()
+require('dotenv').config()
 
 // require express
 const express = require('express')
@@ -10,13 +10,13 @@ const app = express()
 app.use('/places', require('./controllers/places'))
 
 //creating homepage route
-app.get('/', function(req,res) {
-    res.send('Welcome')
+app.get('/', (req,res) => {
+    res.send('Welcome!')
 })
 
 //Wildcard route
-app.get('*', (req, res) =>{
-    res.status(404) .send('<h1>404 Page</h1>')
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 //listen for connections
