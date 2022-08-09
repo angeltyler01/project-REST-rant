5494c78a200818adcf0587918264776064cb7775
Angel Tyler <angeltyler@Angels-MBP.attlocal.net>
2022-08-09 13:25:15 -0700

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 //import router from places.js
 app.use('/places', require('./controllers/places'))
