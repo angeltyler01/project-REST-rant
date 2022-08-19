fb4765da79819c0b8d5dd583cb435ff102894665
Angel Tyler <angeltyler@Angels-MBP.attlocal.net>
2022-08-19 14:48:23 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -40,6 +40,23 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
 
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
+  
+  
+
 
 module.exports = router
 
