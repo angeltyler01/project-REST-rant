a94bac723db04e4864171771ad7b21c2eaa8b1ad
Angel Tyler <angeltyler@Angels-MBP.attlocal.net>
2022-08-19 14:42:00 -0700

    Edit form page stubs work

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 // require express
 const express = require('express')
 
+//require method override
+const methodOverride = require('method-override')
+
 //initialize express
 const app = express()
 
@@ -10,6 +13,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 //import router from places.js
 app.use('/places', require('./controllers/places'))
