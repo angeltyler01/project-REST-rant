152747e7bf7c4c46b34823cacf4f2c80f9ecaf69
Angel Tyler <angeltyler@Angels-MBP.attlocal.net>
2022-08-24 19:26:49 -0700

    Added the edit form

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const Def = require('../default')
+const Def = require('../default.jsx')
 
 function index (data) {
     let placesFormatted = data.places.map((place, index) => {
